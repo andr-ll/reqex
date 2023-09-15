8f4e92d8c28cae2584768830e763630959278ed3
andr-ii <andr.lyt.dev@gmail.com>
2023-09-15 14:56:40 +0200

    release: reqex v1.0.0

---
+++ jest.config.ts
@@ -0,0 +1,27 @@
+export default {
+  preset: 'ts-jest',
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['./src/**/**'],
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/docs/',
+    'types.ts',
+    'types',
+    './src/index.ts',
+    '.test.ts',
+  ],
+  coverageProvider: 'v8',
+  coverageReporters: ['text', 'lcov'],
+  coverageThreshold: {
+    global: {
+      branches: 99,
+      functions: 99,
+      lines: 99,
+      statements: 99,
+    },
+  },
+  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+  verbose: true,
+};
